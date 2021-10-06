172df07f8a44b6944336e1e3eb81157629ffd50f
Navid <navid.anjum@northsouth.edu>
2021-10-06 22:59:18 +0600

    fourth commit

--- components/Layout.js
+++ components/Layout.js
@@ -1,33 +1,37 @@
+import React from 'react'
+import Link from 'next/link'
 import  Navbar  from 'react-bootstrap/Navbar'
 import  Nav  from 'react-bootstrap/Nav'
 import  NavDropdown  from "react-bootstrap/NavDropdown"
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react'
+
 
 
 const Layout = ({children}) => {
     return (
         <>
         <Navbar bg="light" expand="lg" className="mx-5">
-            <Navbar.Brand href="#">
-                <img src="/logo.png" height={33} width={120}></img>
-            </Navbar.Brand>
+            <Link href="/" passHref>
+                <Navbar.Brand>
+                    <img src="/logo.png" height={33} width={120}></img>
+                </Navbar.Brand>
+            </Link>
             <Navbar.Toggle aria-controls="navbarScroll" />
             <Navbar.Collapse id="navbarScroll">
                 <Nav
                 className="ms-auto my-2 my-lg-0"
                 navbarScroll
                 >
-                <Nav.Link href="#action1">Home</Nav.Link>
+                <Link href="/" passHref><Nav.Link>Home</Nav.Link></Link>
                 <NavDropdown title="Products" id="navbarScrollingDropdown">
-                    <NavDropdown.Item href="#action3">Saree</NavDropdown.Item>
-                    <NavDropdown.Item href="#action4">Kurta</NavDropdown.Item>
-                    <NavDropdown.Item href="#action5">Salwar</NavDropdown.Item>
-                    <NavDropdown.Item href="#action5">Pant</NavDropdown.Item>
+                    <Link href="/saree" passHref><NavDropdown.Item>Saree</NavDropdown.Item></Link>
+                    <Link href="/pant" passHref><NavDropdown.Item>Pant</NavDropdown.Item></Link>
+                    <Link href="/kurta" passHref><NavDropdown.Item>Kurta</NavDropdown.Item></Link>
+                    <Link href="/salwar" passHref><NavDropdown.Item>Salwar</NavDropdown.Item></Link>
                 </NavDropdown>
-                <Nav.Link href="#action1">Sale</Nav.Link>
-                <Nav.Link href="#action1">Search</Nav.Link>
-                <Nav.Link href="#action1">Cart</Nav.Link>
+                <Link href="/sale" passHref><Nav.Link>Sale</Nav.Link></Link>
+                <Link href="/search" passHref><Nav.Link>Search</Nav.Link></Link>
+                <Link href="/cart" passHref><Nav.Link>Cart</Nav.Link></Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
