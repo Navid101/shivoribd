276aad2c95960383c253b96b6dc061cf41a79988
Navid <navid.anjum@northsouth.edu>
2021-10-09 01:19:40 +0600

    categories started

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,9 @@ import  Carousel  from 'react-bootstrap/Carousel'
 const index = () => {
   return (
     <>
+    {/* {Carousel Section starts} */}
     <div className="carouselContainer">
-      <Carousel fade>
+      <Carousel fade={true} indicators={false}>
         <Carousel.Item interval={1200}>
           <Image src="/assets/slide_1.webp" height={1080} width={1920}></Image>
         </Carousel.Item>
@@ -20,9 +21,14 @@ const index = () => {
         </Carousel.Item>
       </Carousel>
     </div>
+    {/* {Carousel Section starts} */}
+
+    {/* {Categories section starts} */}
     <section className="container d-flex flex-column justify-content-center align-items-center pt-5">
-      <h2>Categories</h2>
+      <h2>Categories</h2>      
     </section>
+    {/* {Categories section ends} */}
+
     </>
   )
 }
