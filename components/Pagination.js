a015469444d87b2d16f3ebc7899ddcaff4715722
Navid <navid.anjum@northsouth.edu>
2021-10-11 19:20:53 +0600

    pagination start

---
+++ components/Pagination.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import styles from './../styles/Components.module.css'
+const Pagination = ({productsPerPage,totalProducts, paginate}) => {
+    const pageNumbers = [];
+    for(let i = 1; i<=Math.ceil(totalProducts/productsPerPage);i++){
+        pageNumbers.push(i)
+    }
+    return (
+        <div>
+            <ul className="pagination pb-2">
+                {pageNumbers.map(number=>{
+                    return (
+                    <li key={number} className="page-item">
+                        <button onClick={()=>paginate(number)} className={styles.paginationItem}>{number}</button>
+                    </li>
+                    )
+                })}
+            </ul>
+        </div>
+    )
+}
+
+export default Pagination
--- pages/product/[productCategory]/index.js
+++ pages/product/[productCategory]/index.js
@@ -1,23 +1,36 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import  {useRouter}  from 'next/router'
 import Card from '../../../components/Card';
+import Pagination from '../../../components/Pagination';
 import styles from './../../../styles/Product.module.css'
 
-const ids = [1,2,3,4,5,6]
+const products = [1,2,3,4,5,6]
 
 const ProductList = () => {
     const router = useRouter();
     const {productCategory} = router.query;
+    const [currentPage, setCurrentPage] = useState(1);
+    const productsPerPage = 4;
+
+    //Get Current Producsts
+    const indexOFLastProduct = currentPage * productsPerPage;
+    const indexOfFirstProduct = indexOFLastProduct - productsPerPage;
+    const currentProducts = products.slice(indexOfFirstProduct,indexOFLastProduct);   
+
+    //Change Page
+    const paginate = (pageNumber)=> setCurrentPage(pageNumber)
+
     return (
         <div className="container d-flex flex-column justify-content-center align-items-center pt-3">
             <h2>{`${productCategory}`.toUpperCase()}</h2>
             <div className={styles.productsContainer}>
-                {ids.map((id)=>{
+                {currentProducts.map((product)=>{
                     return(
-                    <Card key={id} image={`/assets/webp/${id}.webp`} name="Jamdani Saree Hand Stitched" price={`৳${200}`}></Card>
+                    <Card key={product} image={`/assets/webp/${product}.webp`} name="Jamdani Saree Hand Stitched" price={`৳${200}`}></Card>
                     )
                 })}
             </div>
+            <Pagination productsPerPage={productsPerPage} totalProducts={products.length} paginate={paginate}></Pagination>
         </div>
     )
 }
